95bdbd49971dc13b97eae07cc45cb998010ebbe7
Rayzr522 <rayzr522@gmail.com>
2016-12-05 21:08:42 -0500

    Lightly improved //github

--- src/commands/github.js
+++ src/commands/github.js
@@ -64,13 +64,13 @@ function safeRepo(input) {
 function getInfo(json) {
     return `**${json.full_name}**
 
-\t**Description:** _${json.description}_
+\t**Description:** _${json.description || 'None provided'}_
 \t**Owner:** [${json.owner.login}](${json.owner.html_url})
 \t**Primary Language:** \`${json.language}\` 
 
 \t:house:  [Home page](${json.html_url})  :small_red_triangle_down:  [Downlads](${json.html_url}/releases)  :exclamation:  [Issues](${json.html_url}/issues)
 
-\t:negative_squared_cross_mark:  \`${json.open_issues_count}\` open issues  :star:  \`${json.stargazers_count}\` stargazers  :eyes:  \`${json.watchers_count}\` watchers
+\t:negative_squared_cross_mark:  \`${json.open_issues_count}\` open issues  :star:  \`${json.stargazers_count}\` stargazers  :eyes:  \`${json.subscribers_count || json.watchers_count}\` watchers
 
 
 \tDo \`git clone ${json.clone_url}\` to clone this repository
