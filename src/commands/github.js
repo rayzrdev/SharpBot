1352dbbe28619953e677833b423abe27c698d923
Rayzr522 <rayzr522@gmail.com>
2016-12-05 19:46:19 -0500

    Improved //github MASSIVELY

--- src/commands/github.js
+++ src/commands/github.js
@@ -1,11 +1,81 @@
+const request = require('superagent');
+const utils = require('../utils');
+
 exports.run = function (bot, msg, args) {
-    if (args.length < 1 || args[0].indexOf('/') <= 0) {
-        msg.edit(':no_entry_sign: You must specify a user and a repository!');
+    if (args.length < 1) {
+        msg.edit(':no_entry_sign: You must specify a repository!').then(m => m.delete(2000));
         return;
     }
 
-    msg.edit(`:white_check_mark: \`${args[0]}\`: https://github.com/${args[0]}`);
+    if (args[0].indexOf('/') !== -1) {
+
+        let repo = safeRepo(args[0]);
+
+        msg.edit(`:arrows_counterclockwise:  Loading info for '${repo}'...`);
+
+        request.get(`https://api.github.com/repos/${repo}`, (err, res) => {
+            let json = JSON.parse(res.text);
+            if (json.message === 'Not Found') {
+                msg.edit(':no_entry_sign: That repository could not be found!').then(m => m.delete(2000));
+                return;
+            }
+
+            msg.edit('', { embed: utils.embed('', getInfo(json)) });
+        });
+
+    } else {
+
+        msg.edit(`:arrows_counterclockwise:  Searching for '${args.join(' ')}'...`);
+
+        request.get(`https://api.github.com/search/repositories?q=${args.join('+')}`, (err, res) => {
+            let json = JSON.parse(res.text);
+            if (json.total_count < 1) {
+                msg.edit(`:sob: No results found for '${args.join(' ')}'`).then(m => m.delete(2000));
+                return;
+            }
+
+            msg.delete();
+            msg.channel.sendMessage(':white_check_mark: Top 3 results:');
+
+            for (var i = 0; i < 3; i++) {
+                if (!json.items[i]) {
+                    break;
+                }
+                let item = json.items[i];
+                msg.channel.sendMessage('', { embed: utils.embed('', getInfo(item)) });
+            }
+        });
+
+    }
+
 };
+
+function safeRepo(input) {
+    if (input.indexOf('/') === -1) {
+        return;
+    }
+    let user = input.substr(0, input.indexOf('/'));
+    input = input.substr(input.indexOf('/') + 1);
+    let repo = input.indexOf('/') === -1 ? input : input.substr(0, input.indexOf('/'));
+    console.log(`${user}/${repo}`);
+    return `${user}/${repo}`;
+}
+
+function getInfo(json) {
+    return `**${json.full_name}**
+
+\t**Description:** _${json.description}_
+\t**Owner:** [${json.owner.login}](${json.owner.html_url})
+\t**Primary Language:** \`${json.language}\` 
+
+\t:house:  [Home page](${json.html_url})  :small_red_triangle_down:  [Downlads](${json.html_url}/releases)  :exclamation:  [Issues](${json.html_url}/issues)
+
+\t:negative_squared_cross_mark:  \`${json.open_issues_count}\` open issues  :star:  \`${json.stargazers_count}\` stargazers  :eyes:  \`${json.watchers_count}\` watchers
+
+
+\tDo \`git clone ${json.clone_url}\` to clone this repository
+`;
+}
 
 exports.info = {
     name: 'github',
