b5a558b4e1350ef8bdcc6fab13945d3472a7a5bb
Rayzr522 <rayzr522@gmail.com>
2016-12-07 19:20:28 -0500

    Added a //stats command

---
+++ src/commands/stats.js
@@ -0,0 +1,25 @@
+const utils = require('../utils');
+
+exports.run = function (bot, msg) {
+    msg.edit('', {
+        embed: utils.embed('SharpBot Stats', `
+
+This message will dissappear in 10 seconds.
+
+
+**Servers:** ${bot.guilds.size}
+
+**Users:** ${bot.users.size}
+
+**RAM:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+
+`)
+    })
+        .then(m => m.delete(10000));
+};
+
+exports.info = {
+    name: 'stats',
+    usage: 'stats',
+    description: 'Shows you stats about SharpBot'
+};
