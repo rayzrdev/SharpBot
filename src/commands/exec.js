79acc1bfda6c39d14d90ca9d65b1a06498718123
Rayzr522 <rayzr522@gmail.com>
2017-01-23 12:56:36 -0500

    Added privacy and ANSI filter to //exec

--- src/commands/exec.js
+++ src/commands/exec.js
@@ -1,9 +1,16 @@
 const {exec} = require('child_process');
+const username = require('os').userInfo().username;
+
+const clean = function (data) {
+    return data.toString()
+        .replace(new RegExp(username, 'g'), '<Hidden>')
+        .replace(/\[[0-9]*m/g, '');
+};
 
 exports.run = (bot, msg, args) => {
     var ps = exec(args.join(' '));
-    ps.stdout.on('data', data => msg.channel.sendCode('diff', data.toString()));
-    ps.stderr.on('data', data => msg.channel.sendCode('diff', data.toString()));
+    ps.stdout.on('data', data => msg.channel.sendCode('diff', clean(data)));
+    ps.stderr.on('data', data => msg.channel.sendCode('diff', clean(data)));
 };
 
 exports.info = {
