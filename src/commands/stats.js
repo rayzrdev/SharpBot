c1d29e801192bd9800892c2fbac3190a058d9656
Rayzr522 <rayzr522@gmail.com>
2017-02-21 12:55:58 -0500

    Updated //stats

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,20 +1,35 @@
+const Discord = require('discord.js');
 const utils = require('../utils');
 
 exports.run = function (bot, msg) {
     msg.edit('', {
-        embed: utils.embed('SharpBot Stats', `
-
-This message will dissappear in 10 seconds.
-
-
-**Servers:** ${bot.guilds.size}
-
-**Users:** ${bot.users.size}
-
-**RAM:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-
-`)
-    }).then(m => m.delete(10000));
+        embed: utils.embed('SharpBot Stats', '***This message will dissappear in 15 seconds.***', [
+            {
+                name: 'Owner:',
+                value: `<@${bot.user.id}>`,
+            },
+            {
+                name: 'Discord.js',
+                value: `v${Discord.version}`,
+            },
+            {
+                name: 'Mem Usage:',
+                value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+            },
+            {
+                name: 'Users:',
+                value: `${bot.users.size.toLocaleString()}`,
+            },
+            {
+                name: 'Servers:',
+                value: `${bot.guilds.size.toLocaleString()}`,
+            },
+            {
+                name: 'Channels:',
+                value: `${bot.channels.size.toLocaleString()}`,
+            },
+        ], { inline: true })
+    }).then(m => m.delete(15000));
 };
 
 exports.info = {
