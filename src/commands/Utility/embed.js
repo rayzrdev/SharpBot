ece2ceb6f432b4cc46d85e549efd0b779de4fa35
Rayzr522 <rayzr522@gmail.com>
2017-03-15 15:43:17 -0400

    Added -r to //embed

--- src/commands/Utility/embed.js
+++ src/commands/Utility/embed.js
@@ -3,19 +3,27 @@ exports.run = (bot, msg, args) => {
         throw 'You must specify some text!';
     }
 
-    var parsed = bot.utils.parseArgs(args, ['f', 'c:']);
+    var parsed = bot.utils.parseArgs(args, ['f', 'c:', 'r']);
+
+    var color = parsed.options.c;
+    if (parsed.options.r && msg.guild && msg.guild.members) {
+        var member = msg.guild.members.get(msg.author.id);
+        if (member) {
+            color = member.highestRole.hexColor;
+        }
+    }
 
     msg.delete();
     msg.channel.sendEmbed(
         bot.utils.embed('', parsed.leftover.join(' '), [], {
             footer: !!parsed.options.f,
-            color: parsed.options.c
+            color
         })
     );
 };
 
 exports.info = {
     name: 'embed',
-    usage: 'embed [-f] [-c <color>] [text]',
+    usage: 'embed [-f] [-r] [-c <color>] [text]',
     description: 'Sends a message via embeds'
 };
