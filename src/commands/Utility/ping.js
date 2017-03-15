6774cc381c04595962fb9e050469b4013722f783
Rayzr522 <rayzr522@gmail.com>
2017-03-15 15:43:07 -0400

    Added //ping

---
+++ src/commands/Utility/ping.js
@@ -0,0 +1,18 @@
+exports.run = (bot, msg) => {
+    msg.edit(':stopwatch: Ping').then(m => {
+        var time = msg.editedTimestamp - msg.createdTimestamp;
+        bot.utils.playAnimation(m, 500, [
+            ':stopwatch: __P__ing',
+            ':stopwatch: __Pi__ng',
+            ':stopwatch: __Pin__g',
+            ':stopwatch: __Ping__',
+            `:stopwatch: ***Pong!*** \`${time}ms\``
+        ]);
+    });
+};
+
+exports.info = {
+    name: 'ping',
+    usage: 'ping',
+    description: 'Pings the bot'
+};
