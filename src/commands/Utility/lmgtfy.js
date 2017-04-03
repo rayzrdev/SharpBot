19fed2ab46f9390123b86e23bfc2f5b75873f5ba
Rayzr522 <rayzr522@gmail.com>
2017-04-03 10:53:35 -0400

    Added toggle for Internet Explainer

--- src/commands/Utility/lmgtfy.js
+++ src/commands/Utility/lmgtfy.js
@@ -2,11 +2,21 @@ exports.run = (bot, msg, args) => {
     if (args.length < 1) {
         throw 'You must provide something to search for!';
     }
-    msg.edit(`**Wow!** :arrow_right: http://www.lmgtfy.com/?iie=1&q=${args.join('+')}`);
+
+    var parsed = bot.utils.parseArgs(args, ['i']);
+
+    msg.edit(`**Wow!** :arrow_right: http://www.lmgtfy.com/?iie=${parsed.options.i ? 1 : 0}&q=${parsed.leftover.join('+')}`);
 };
 
 exports.info = {
     name: 'lmgtfy',
     usage: 'lmgtfy [search text]',
-    description: 'Links to LMGTFY with the given search text'
+    description: 'Links to LMGTFY with the given search text',
+    options: [
+        {
+            name: '-i',
+            usage: '-i',
+            description: 'Enables Internet Explainer'
+        }
+    ]
 };  
