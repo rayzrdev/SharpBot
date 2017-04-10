484c2d3ecefcc65b32a65324c182e5a6b74ce779
Rayzr522 <rayzr522@gmail.com>
2017-04-10 12:15:56 -0400

    Tweaked //shortcuts

--- src/commands/Utility/shortcuts.js
+++ src/commands/Utility/shortcuts.js
@@ -2,20 +2,22 @@ exports.run = (bot, msg, args) => {
     if (args.length < 1) {
         return bot.db.entries().then(entries => {
             var shortcuts = entries.filter(e => e.key.startsWith('shortcuts.')).map(e => e.value);
-
-            msg.edit(shortcuts.map(sc => `**${sc.name}:** \`${sc.command}\``).join('\n')).then(m => m.delete(5000));
+            if (shortcuts.length < 1) {
+                throw 'You have no shortcuts!';
+            }
+            msg.edit(shortcuts.map(sc => `**${sc.name}:** \`${sc.command}\``).join('\n')).then(m => m.delete(15000));
         });
     }
 
     if (/a(dd)?|c(reate)?/i.test(args[0])) {
         if (args.length < 3) {
-            return msg.edit(`:no_entry_sign: Usage: \`${bot.config.prefix}shortcut add <id> <command>\``).then(m => m.delete(2000));
+            throw `Usage: \`${bot.config.prefix}shortcut add <id> <command>\``;
         }
         let id = args[1];
         var command = args.slice(2).join(' ');
         bot.db.get(`shortcuts.${id}`).then(sc => {
             if (sc) {
-                return msg.edit(`:no_entry_sign: The shortcut \`${id}\` already exists!`).then(m => m.delete(2000));
+                throw `The shortcut \`${id}\` already exists!`;
             }
             bot.db.put(`shortcuts.${id}`, { name: id, command }).then(() => {
                 msg.edit(`:white_check_mark: Created shortcut \`${id}\`!`).then(m => m.delete(2000));
@@ -23,7 +25,7 @@ exports.run = (bot, msg, args) => {
         });
     } else if (/d(el(ete)?)?|r(em(ove)?)?/i.test(args[0])) {
         if (args.length < 2) {
-            return msg.edit(`:no_entry_sign: Usage: \`${bot.config.prefix}shortcut remove <id>\``).then(m => m.delete(2000));
+            throw `Usage: \`${bot.config.prefix}shortcut remove <id>\``;
         }
         let id = args[1];
         bot.db.delete(`shortcuts.${id}`).then(() => {
