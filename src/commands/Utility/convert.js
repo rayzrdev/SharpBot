e34a4a5020f40287b265f66b387e4f208f0339fe
Abyss <abyss@voidinput.com>
2017-04-10 20:01:41 -0400

    Convert module
    Convert using duckduckgo.

--- src/commands/Utility/convert.js
+++ src/commands/Utility/convert.js
@@ -1,30 +1,63 @@
-// const RichEmbed = require('discord.js').RichEmbed;
+const RichEmbed = require('discord.js').RichEmbed;
+const got = require('got');
 
 exports.run = function (bot, msg, args) {
     if (args.length < 1) {
         throw 'You must specify something to convert';
     }
 
-    // converter.convert(args.join(' '), function(err, value, str) {
-    //     msg.delete();
-    //
-    //     var bc = '\`\`\`';
-    //
-    //     var message = new RichEmbed()
-    //             .setColor(bot.utils.randomColor())
-    //             .setTitle('Conversion')
-    //             .addField('Input:', `${bc} ${args.join(' ')} ${bc}`)
-    //             .addField('Output:', `${bc} ${value} ${str} ${bc}`)
-    //             .addField('Error:', `${bc} ${err} ${bc}`);
-    //
-    //     msg.channel.sendEmbed(message);
-    // });
+    var input = args.join(' ');
+    var url = `https://api.duckduckgo.com/?q=${encodeURIComponent(input)}&format=json`;
+    msg.edit(':arrows_counterclockwise:  Loading conversion...');
 
+    got(url).then(res => {
+
+        var data = JSON.parse(res.body);
+
+        var answer = data['Answer'];
+        var message;
+
+        if (data['AnswerType'] === 'conversions') {
+            msg.delete();
+
+            answer = answer.replace('=', '➔');
+
+            message = new RichEmbed()
+                .setColor(bot.utils.randomColor())
+                .addField('Conversion:', answer);
+
+            msg.channel.sendEmbed(message);
+
+        } else if (data['AnswerType'] === 'timezone_converter') {
+            msg.delete();
+
+            var matches = input.match(/(.*?)\s*(to|in)\s*(.*)/);
+            var prefix;
+
+            if (matches) {
+                prefix = matches[1];
+            } else {
+                prefix = input;
+            }
+
+            message = new RichEmbed()
+                .setColor(bot.utils.randomColor())
+                .addField('Conversion:', `${prefix} ➔ ${answer}`);
+
+            msg.channel.sendEmbed(message);
+        } else {
+            msg.error(`No conversion found for ${input}`);
+        }
+
+    }).catch(err => {
+        msg.error('DuckDuckGo returned an error. See console.');
+        console.log(err);
+    });
 };
 
 exports.info = {
     name: 'convert',
     usage: 'convert <measurement> to <unit>',
-    description: 'converts practically any unit, or timezones to another',
+    description: 'converts practically any unit, or timezones to another, using DuckDuckGo searches',
     credits: '<@136641861073764352>' // Abyss#0473
 };
