1268a8eb9b4cd21c7f2723cf8de6b8cf60019a46
Carbowix <killeryoyo01@gmail.com>
2017-04-11 22:41:51 +0200

    Update ytsearch.js
    Fixed some Bugs

--- src/commands/Utility/ytsearch.js
+++ src/commands/Utility/ytsearch.js
@@ -1,14 +1,20 @@
-exports.run = (bot, msg, args) => {
-    msg.channel.sendMessage(":mag_right: **Searching**...").then( m => {
+var yts = require('youtube-scrape')
 
+function rnd_selection(base)
+{
+	return String(arguments[Math.floor(Math.random()*arguments.length)]);
+}
+
+exports.run = (bot, msg, args) => {
+ 
 yts(`${args.join(' ')}`).then((data) => {
- m.edit('', {
+ msg.edit('', {
 				embed: {
 					color: 
 					rnd_selection(3447003, 14365491, 3201849, 13818670, 13577435, 7089371, 14383916),
 					author: {
-						name: message.author.username,
-						icon_url: message.author.displayAvatarURL
+						name: msg.author.username,
+						icon_url: msg.author.displayAvatarURL
 					},
           image: {url: data.results[0].thumbnail},
           description: `[${data.results[0].title}](${data.results[0].link})`,
@@ -27,11 +33,10 @@ yts(`${args.join(' ')}`).then((data) => {
 				}
 			});
    })	 
-})
 }
 
 exports.info = {
     name: 'yt',
-    usage: 'yt0 <terms>',
+    usage: 'yt <terms>',
     description: 'Fetches a Youtube video Details'
 };
