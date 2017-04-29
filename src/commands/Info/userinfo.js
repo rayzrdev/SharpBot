31c314ae048c083d675103ae2e970ee4e6a27b6d
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-04-28 23:29:44 -0400

    Fixed joinedAt

--- src/commands/Info/userinfo.js
+++ src/commands/Info/userinfo.js
@@ -7,19 +7,20 @@ dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
 
 
 exports.run = function (bot, msg) {
-    
+
     if (msg.mentions.users.size < 1) {
         throw '@mention someone to find the info';
     }
-    
+
+    let member = msg.guild.member(msg.mentions.users.first());
     let user = msg.mentions.users.first();
-    
+
     const millisCreated = new Date().getTime() - user.createdAt.getTime();
     const daysCreated = millisCreated/1000/60/60/24;
-    
-    const millisJoined = new Date().getTime() - msg.member.joinedAt.getTime();
+
+    const millisJoined = new Date().getTime() - member.joinedAt.getTime();
     const daysJoined = millisJoined/1000/60/60/24;
-    
+
     let embed = bot.utils.embed(
         `${user.username}`,
         stripIndents`
@@ -31,7 +32,7 @@ exports.run = function (bot, msg) {
             },
             {
                 name: 'Game',
-                value: `${(msg.mentions.users.first().presence.game && msg.mentions.users.first().presence.game && msg.mentions.users.first().presence.game.name) || 'Not playing a game.'}`,
+                value: `${(user.presence.game && user.presence.game && user.presence.game.name) || 'Not playing a game.'}`,
             },
             {
                 name: 'Created On',
@@ -41,15 +42,15 @@ exports.run = function (bot, msg) {
                 name: 'Days Since Creation',
                 value: `${daysCreated.toFixed(0)}`,
             },
-            { 
+            {
                 name: 'Joined On',
-                value: `${dateFormat(user.joinedTimestamp)}`,
+                value: `${dateFormat(member.joinedAt)}`,
             },
             {
                 name: 'Days Since Joined',
                 value: `${daysJoined.toFixed(0)}`,
             },
-        ], 
+        ],
         {
             inline: true,
         });
@@ -58,10 +59,10 @@ exports.run = function (bot, msg) {
         embed.setThumbnail(`${user.avatarURL}`);
     }
 
-    msg.editEmbed(embed).then(m => m.delete(30000)); 
+    msg.editEmbed(embed).then(m => m.delete(30000));
 
 };
-        
+
 exports.info = {
     name: 'userinfo',
     usage: 'userinfo',
