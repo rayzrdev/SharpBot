d7de44690044c810b8e227fed4d6167be1658699
Brandon <brandonb2000@users.noreply.github.com>
2017-04-29 21:31:56 +1000

    Added Roles, User ID, and guild check to userinfo

--- src/commands/Info/userinfo.js
+++ src/commands/Info/userinfo.js
@@ -4,25 +4,30 @@ const stripIndents = require('common-tags').stripIndents;
 const now = new Date();
 dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
 
-
-
 exports.run = function (bot, msg) {
 
+//Makes sure user mentions a user to get info for
     if (msg.mentions.users.size < 1) {
         throw '@mention someone to find the info';
+    }
+//Makes sure command is sent in a guild
+    if (!msg.guild) {
+        throw 'This can only be used in a guild!';
     }
 
     let member = msg.guild.member(msg.mentions.users.first());
     let user = msg.mentions.users.first();
 
+//How long ago the account was created
     const millisCreated = new Date().getTime() - user.createdAt.getTime();
     const daysCreated = millisCreated/1000/60/60/24;
 
+//How long about the user joined the server
     const millisJoined = new Date().getTime() - member.joinedAt.getTime();
     const daysJoined = millisJoined/1000/60/60/24;
 
     let embed = bot.utils.embed(
-        `${user.username}`,
+        `${user.username}#${msg.mentions.users.first().discriminator}`,
         stripIndents`
         ***This message will dissappear in 30 seconds.***`,
         [
@@ -50,9 +55,15 @@ exports.run = function (bot, msg) {
                 name: 'Days Since Joined',
                 value: `${daysJoined.toFixed(0)}`,
             },
+            {
+                name: 'Roles',
+                value: `${member.roles.map(r => r.name).reverse().join(', ')}`,
+                inline: false,
+            },
         ],
         {
             inline: true,
+            footer: `User ID: ${user.id}`
         });
 
     if (msg.guild.iconURL != null) {
