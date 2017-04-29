65fea10e916e05b04466fd4c9197b3962b35be83
Rayzr522 <rayzr522@gmail.com>
2017-04-29 09:25:25 -0400

    Changed //ping visuals

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,18 +1,34 @@
-exports.run = (bot, msg) => {
-    msg.edit(':stopwatch: Ping').then(m => {
-        let time = msg.editedTimestamp - msg.createdTimestamp;
-        bot.utils.playAnimation(m, 500, [
-            ':stopwatch: __P__ing',
-            ':stopwatch: __Pi__ng',
-            ':stopwatch: __Pin__g',
-            ':stopwatch: __Ping__',
-            `:stopwatch: ***Pong!*** \`${time}ms\``
-        ]);
+exports.run = (bot, msg, args) => {
+    let parsed = bot.utils.parseArgs(args, ['o']);
+
+    if (parsed.options.o) {
+        return msg.edit(':stopwatch: Ping').then(m => {
+            let time = msg.editedTimestamp - msg.createdTimestamp;
+            bot.utils.playAnimation(m, 500, [
+                ':stopwatch: __P__ing',
+                ':stopwatch: __Pi__ng',
+                ':stopwatch: __Pin__g',
+                ':stopwatch: __Ping__',
+                `:stopwatch: ***Pong!*** \`${time}ms\``
+            ]);
+        });
+    }
+
+    msg.edit(':thinking: Ping').then(() => {
+        msg.edit(`:stopwatch: Pong! \`${msg.editedTimestamp - msg.createdTimestamp}ms\``)
+            .then(m => m.delete(5000));
     });
 };
 
 exports.info = {
     name: 'ping',
-    usage: 'ping',
-    description: 'Pings the bot'
+    usage: 'ping [-o]',
+    description: 'Pings the bot',
+    options: [
+        {
+            name: '-o',
+            usage: '-o',
+            description: 'Shows the old ping message (animated)'
+        }
+    ]
 };
