d1f8039e8715b735928ef6c3f5d383e9adde5e63
LucasPMagno <lucaspmagno@gmail.com>
2017-05-17 16:19:51 -0400

    Fixed warnings
    ...

--- src/commands/Fun/leet.js
+++ src/commands/Fun/leet.js
@@ -1,16 +1,16 @@
 exports.run = function(bot, message, args) {
 
     if (args.length < 1) {
-        throw 'Provide text to be leeted.'
+        throw 'Provide text to be leeted.';
     }
 
 
     const parsed = args.join(' ').replace(/[a-z]/g, function f(a) {
-        return "4BCD3F6H1JKLMN0PQR57" [parseInt(a, 36) - 10] || a.replace(/[a-t]/gi, f);
+        return '4BCD3F6H1JKLMN0PQR57' [parseInt(a, 36) - 10] || a.replace(/[a-t]/gi, f);
     });
 
-    message.channel.send(parsed.toLowerCase())
-}
+    message.channel.send(parsed.toLowerCase());
+};
 
 exports.info = {
     name: 'leet',
