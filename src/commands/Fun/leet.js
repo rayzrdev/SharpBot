4150ed2291e210cf844a621b347eaa99df7bd1e9
Carbowix <killeryoyo01@gmail.com>
2017-05-17 07:20:56 +0200

    Fixed some things

--- src/commands/Fun/leet.js
+++ src/commands/Fun/leet.js
@@ -1,11 +1,11 @@
 exports.run = function(bot, message, args) {
 
-    if (params.length < 1) {
-    throw 'provide text to be leeted.'
+    if (args.length < 1) {
+        throw 'Provide text to be leeted.'
     }
 
 
-    var parsed = args.join(' ').replace(/[a-z]/g, function f(a) {
+    const parsed = args.join(' ').replace(/[a-z]/g, function f(a) {
         return "4BCD3F6H1JKLMN0PQR57" [parseInt(a, 36) - 10] || a.replace(/[a-t]/gi, f);
     });
 
@@ -15,5 +15,5 @@ exports.run = function(bot, message, args) {
 exports.info = {
     name: 'leet',
     usage: 'leet <text>',
-    description: 'Talk like true Gamers'
+    description: 'Talk like true gamers'
 };
