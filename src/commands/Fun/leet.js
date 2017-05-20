df0e824ca6fafcd54cef692b362ddb810d19d8d3
LucasPMagno <lucaspmagno@gmail.com>
2017-05-20 14:04:12 -0400

    Well, leet doesn't delete itself
    I wonder why...

--- src/commands/Fun/leet.js
+++ src/commands/Fun/leet.js
@@ -8,7 +8,7 @@ exports.run = function(bot, message, args) {
     const parsed = args.join(' ').replace(/[a-z]/g, function f(a) {
         return '4BCD3F6H1JKLMN0PQR57' [parseInt(a, 36) - 10] || a.replace(/[a-t]/gi, f);
     });
-
+    // message.delete();
     message.channel.send(parsed.toLowerCase());
 };
 
