3c1f23e172d1fbf5f2cf80ba0f35214db79beefe
LucasPMagno <lucaspmagno@gmail.com>
2017-05-19 20:02:00 -0400

    Selfdestructing messages yay
    Poof.

---
+++ src/commands/Fun/selfdestruct.js
@@ -0,0 +1,27 @@
+exports.run = function (bot, msg, args) {
+    let parsed = bot.utils.parseArgs(args, ['t:']);
+    let time = 5000;
+
+    if(parsed.options.t) {
+        time = parsed.options.t;
+    }
+
+    msg.edit(parsed.leftover.join(' ')).then(m => {
+        setInterval(() => {
+            m.delete();
+        }, time);
+    });
+};
+
+exports.info = {
+    name: 'selfdestruct',
+    usage: 'selfdestruct <message> [-t time]',
+    description: 'Deletes the message after a specified time, 5 seconds if not specified',
+    options: [
+        {
+            name: '-t',
+            usage: '-t <time>',
+            description: 'Sets the time for the message to be deleted'
+        }
+    ]
+};
