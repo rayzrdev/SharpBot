6286d67872e4e8e0bb4bcfc3c3363bab7cf57bcf
Doxylamin <doxylamin@pxls.space>
2017-05-21 13:06:16 +0200

    self-destructing messages with configurable delay & styling.

---
+++ src/commands/Fun/destruct.js
@@ -0,0 +1,52 @@
+exports.run = function(bot,msg,args) {
+
+    let parsedArgs = bot.utils.parseArgs(args, ['d:','s:']);
+
+    if(parsedArgs.leftover.length < 1) {
+        throw 'Please provide a secret message';
+    }
+
+    let text = parsedArgs.leftover;
+    let message = text.join(' ');
+    let delay = isNaN(parsedArgs.options.d) ? 5000 : parsedArgs.options.d;
+    let style = (typeof parsedArgs.options.s === 'string') ? parsedArgs.options.s : 'plain';
+
+    msg.delete();
+
+    switch(style) {
+    case 'embed':
+        message = bot.utils.embed(
+            `This message self-destructs in ${delay/1000} seconds.`,
+            `${message}`,
+            [],
+            {
+                inline: true,
+                footer: 'Secret Message',
+                color: [255, 0, 0]
+            }
+        );
+        msg.channel.sendEmbed(message).then(m => { m.delete(delay); });
+        break;
+    case 'inline':
+        message = `*This message self-destructs in ${delay/1000} seconds.*\n${message}`;
+        msg.channel.send(message).then(m => { m.delete(delay); });
+        break;
+    case 'code':
+        message = `*This message self-destructs in ${delay/1000} seconds.*\n\`\`\`${message}\`\`\``;
+        msg.channel.send(message).then(m => { m.delete(delay); });
+        break;
+    default:
+        message = `${message}`;
+        msg.channel.send(message).then(m => { m.delete(delay); });
+        break;
+    }
+
+};
+
+
+exports.info = {
+    name: 'destruct',
+    usage: 'destruct [-d delay in ms]  [-s <embed|inline|code>] <message>',
+    description: 'creates a self-destructing message',
+    credits: '<@140541588915879936>' // Doxylamin#4539
+};
