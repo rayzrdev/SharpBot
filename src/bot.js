c8b4f103fdf56b6cd61c5e414ddaa96f9e1ea083
Yoni Lerner <yonilerner@gmail.com>
2017-05-28 04:40:44 -0700

    Dont launch user straight into config on failed creds

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,7 @@ const XPDB = require('xpdb');
 const readline = require('readline');
 const didYouMean = require('didyoumean2');
 const stripIndents = require('common-tags').stripIndents;
+const chalk = require('chalk');
 
 const Managers = require('./managers');
 
@@ -160,8 +161,8 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', err => {
     // Force the user to reconfigure if their token is invalid
     if (err.message === 'Incorrect login details were provided.') {
-        logger.severe(err.message);
-        configManager.load(true);
+        logger.severe(`${err.message} Please reconfigure with ${chalk.green('yarn run config')}`);
+        process.exit(666);
     } else {
         logger.severe('Uncaught Promise error: \n' + err.stack);
     }
