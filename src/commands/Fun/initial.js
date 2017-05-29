52a80e9524d28e242f17c1c18810228046cbaaf0
Yoni Lerner <yonilerner@gmail.com>
2017-05-28 21:07:37 -0700

    Some tweaks based on comments

--- src/commands/Fun/initial.js
+++ src/commands/Fun/initial.js
@@ -1,6 +1,6 @@
 exports.run = function(bot, msg, args) {
-    if (args.length < 1) {
-        throw new Error('You must input text to be transformed.');
+    if (args.length === 0) {
+        throw 'You must input text to be transformed.';
     }
     msg.edit(args.map(arg => arg[0].toUpperCase() + arg.slice(1).toLowerCase()).join(' '));
 };
