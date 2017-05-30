6aa7d00898d13be135d15f594c65122182d5cccd
LucasPMagno <lucaspmagno@gmail.com>
2017-05-29 20:25:41 -0400

    Eval improvements
    Now uses embeds and sends hastebin links for outputs larger than 1500 characters (It could be closer to the 2k limit, but I wanted to make sure that you can have very long inputs outputs.)

--- src/commands/Utility/eval.js
+++ src/commands/Utility/eval.js
@@ -1,3 +1,6 @@
+const got = require('got');
+const url = require('url');
+const stripIndents = require('common-tags').stripIndents;
 exports.run = (bot, msg, args) => {
     let parsed = bot.utils.parseArgs(args, ['l:']);
     let lang = parsed.options.l || '';
@@ -6,22 +9,45 @@ exports.run = (bot, msg, args) => {
 
     try {
         let evaled = eval(code);
-        if (typeof evaled !== 'string')
+        if (typeof evaled !== 'string') {
             evaled = require('util').inspect(evaled);
-        msg.channel.sendMessage(`\`\`\`${lang}\n${clean(evaled)}\n\`\`\``);
+        }
+        msg.delete();
+        let output = clean(evaled).replace(bot.token, 'BOT_TOKEN' + String.fromCharCode(8203));
+        got.post(url.resolve('https://hastebin.com', 'documents'), {
+            body: output,
+            json: true,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        }).then(res => {
+            if (!res.body || !res.body.key) {
+                return 'Failed to upload, no key was returned!';
+            }
+            let output2 = res.body.key || res.body;
+            msg.channel.send({
+                embed: bot.utils.embed('', stripIndents`
+                **Input:**\n\`\`\`js\n${code}\n\`\`\`
+                **Output:**${evaled.split('').length < 1900 ? `\n\`\`\`${lang}\n${output}\n\`\`\`` : `\nhttps://hastebin.com/${output2}\n`}
+            `)});
+        });
     } catch (err) {
-        msg.channel.sendMessage(`:x: Error! \`\`\`xl\n${clean(err)}\n\`\`\``).then(m => m.delete(15000));
-    }
-
-    function clean(text) {
-        if (typeof (text) === 'string') {
-            return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203)).replace(bot.token, 'BOT_TOKEN' + String.fromCharCode(8203));
-        }
-        else {
-            return text;
-        }
+        msg.delete();
+        msg.channel.send({
+            embed: bot.utils.embed('', `**Input:**\n\`\`\`js\n${code}\n\`\`\`\n:x: **Error!**\n\`\`\`xl\n${clean(err)}\n\`\`\``, [], {
+                color: '#ff0000'
+            })}).then(m => m.delete(15000));
     }
 };
+
+function clean(text) {
+    if (typeof (text) === 'string') {
+        return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+    }
+    else {
+        return text;
+    }
+}
 
 exports.info = {
     name: 'eval',
