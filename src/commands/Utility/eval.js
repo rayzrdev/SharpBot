2885d60f27c811f2cde354afd6789a1c23c537d2
Yoni Lerner <yonilerner@gmail.com>
2017-06-08 01:46:55 -0700

    When using eval command, return resolved promise value, not promise itself

--- src/commands/Utility/eval.js
+++ src/commands/Utility/eval.js
@@ -6,43 +6,52 @@ exports.run = (bot, msg, args) => {
     let code = parsed.leftover.join(' ');
 
     try {
-        let evaled = eval(code);
-        if (typeof evaled !== 'string') {
-            evaled = require('util').inspect(evaled);
-            if (!lang) {
-                lang = 'javascript';
-            }
-        }
-        msg.delete();
-        let output = clean(evaled).replace(bot.token, 'BOT_TOKEN' + String.fromCharCode(8203));
+        Promise.resolve(eval(code))
+            .then(evaled => {
+                if (typeof evaled !== 'string') {
+                    evaled = require('util').inspect(evaled);
+                    if (!lang) {
+                        lang = 'javascript';
+                    }
+                }
+                msg.delete();
+                let output = clean(evaled).replace(bot.token, 'BOT_TOKEN' + String.fromCharCode(8203));
 
-        bot.utils.hastebinUpload(output).then(({url}) => {
-            if (!url) {
-                return 'Failed to upload, no key was returned!';
-            }
-            msg.channel.send({
-                embed: bot.utils.embed('', stripIndents`
-                **Input:**\n\`\`\`js\n${code}\n\`\`\`
-                **Output:**${output.length < 1500 ? `\n\`\`\`${lang}\n${output}\n\`\`\`` : `\n${url}\n`}
-            `)});
+                bot.utils.hastebinUpload(output).then(({url}) => {
+                    if (!url) {
+                        return 'Failed to upload, no key was returned!';
+                    }
+                    msg.channel.send({
+                        embed: bot.utils.embed('', stripIndents`
+                        **Input:**\n\`\`\`js\n${code}\n\`\`\`
+                        **Output:**${output.length < 1500 ? `\n\`\`\`${lang}\n${output}\n\`\`\`` : `\n${url}\n`}
+                    `)});
 
-            if (output.length > 1500 && parsed.options.i) {
-                bot.utils.sendLarge(msg.channel, output, {
-                    prefix: '```' + lang + '\n',
-                    suffix: '```',
-                    cutOn: ',',
-                    cutAfter: true
+                    if (output.length > 1500 && parsed.options.i) {
+                        bot.utils.sendLarge(msg.channel, output, {
+                            prefix: '```' + lang + '\n',
+                            suffix: '```',
+                            cutOn: ',',
+                            cutAfter: true
+                        });
+                    }
                 });
-            }
-        });
+            })
+            .catch(err => {
+                errorHandler(msg, bot, code, err);
+            });
     } catch (err) {
-        msg.delete();
-        msg.channel.send({
-            embed: bot.utils.embed('', `**Input:**\n\`\`\`js\n${code}\n\`\`\`\n:x: **Error!**\n\`\`\`xl\n${clean(err)}\n\`\`\``, [], {
-                color: '#ff0000'
-            })}).then(m => m.delete(15000));
+        errorHandler(msg, bot, code, err);
     }
 };
+
+function errorHandler(msg, bot, code, err) {
+    msg.delete();
+    msg.channel.send({
+        embed: bot.utils.embed('', `**Input:**\n\`\`\`js\n${code}\n\`\`\`\n:x: **Error!**\n\`\`\`xl\n${clean(err)}\n\`\`\``, [], {
+            color: '#ff0000'
+        })}).then(m => m.delete(15000));
+}
 
 function clean(text) {
     if (typeof (text) === 'string') {
