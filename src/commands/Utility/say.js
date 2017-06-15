af59b3f2699864f78fb2fbae1c1914ffe8e19580
Rayzr522 <rayzr522@gmail.com>
2017-06-15 15:35:00 -0400

    Added channel option to //say

--- src/commands/Utility/say.js
+++ src/commands/Utility/say.js
@@ -1,14 +1,38 @@
 exports.run = (bot, msg, args) => {
-    if (args.length < 1) {
+    const parsed = bot.utils.parseArgs(args, ['c:']);
+    const { leftover } = parsed;
+
+    if (leftover.length < 1) {
         throw 'You must put something to say!';
     }
 
+    let channel = msg.channel;
+    if (parsed.options.c) {
+        const id = parsed.options.c.match(/\d{18}/)[0];
+
+        if (!id) {
+            throw 'Invalid channel!';
+        }
+
+        channel = bot.channels.get(id);
+        if (!channel) {
+            throw 'That channel could not be found!';
+        }
+    }
+
     msg.delete();
-    msg.channel.send(args.join(' '));
+    channel.send(leftover.join(' '));
 };
 
 exports.info = {
     name: 'say',
     usage: 'say <message>',
-    description: 'Says the message you put. Useful for shortcuts.'
+    description: 'Says the message you put. Useful for shortcuts.',
+    options: [
+        {
+            name: '-c',
+            usage: '-c <channel|channel ID>',
+            description: 'Specifies a specific channel to send the message in'
+        }
+    ]
 };
