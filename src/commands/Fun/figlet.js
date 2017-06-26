3b9c8f522a9f8ed0f924e31ec88e00d802aa3eb1
Rayzr522 <rayzr522@gmail.com>
2017-06-26 12:00:45 -0400

    Updated //figlet with support for changing/listing fonts

--- src/commands/Fun/figlet.js
+++ src/commands/Fun/figlet.js
@@ -1,15 +1,40 @@
 const figlet = require('figlet');
 
 exports.run = (bot, msg, args) => {
-    if (args.length < 1) {
+    // -l -- List all fonts
+    // -f <font> -- Set font
+    const parsed = bot.utils.parseArgs(args, ['l', 'f:']);
+
+    if (parsed.options.l) {
+        bot.utils.gistUpload(figlet.fontsSync().join('\n'), 'txt').then(({ url }) => {
+            if (!url) {
+                return msg.error('Failed to upload fonts list!');
+            }
+
+            msg.edit({
+                embed: bot.utils.embed('Available Fonts', `A list of available fonts can be found [here](${url}).`)
+            }).then(m => m.delete(5000));
+        });
+        return;
+    }
+
+    if (parsed.leftover.length < 1) {
         throw 'You must provide some text to render!';
+    }
+
+    const options = {};
+
+    if (parsed.options.f) {
+        options.font = parsed.options.f;
     }
 
     msg.delete();
 
-    figlet.text(args.join(' '), (err, res) => {
-        if (err)
+    figlet.text(parsed.leftover.join(' '), options, (err, res) => {
+        if (err) {
             return msg.error(err);
+        }
+
         msg.channel.sendMessage(`\`\`\`\n${res}\n\`\`\``);
     });
 
@@ -18,5 +43,16 @@ exports.run = (bot, msg, args) => {
 exports.info = {
     name: 'figlet',
     usage: 'figlet <text>',
-    description: 'Renders fancy ASCII text'
+    description: 'Renders fancy ASCII text',
+    options: [
+        {
+            name: '-f',
+            usage: '-f <font>',
+            description: 'Sets the font to use'
+        },
+        {
+            name: '-l',
+            description: 'Lists all available fonts'
+        }
+    ]
 };
