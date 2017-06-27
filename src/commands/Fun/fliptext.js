f5bdd1f7b159eb54c186b0601b252212fac2bf7b
Rayzr522 <rayzr522@gmail.com>
2017-06-27 17:29:50 -0400

    Added //fliptext

---
+++ src/commands/Fun/fliptext.js
@@ -0,0 +1,22 @@
+const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>?@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
+// Start with the character '!'
+const OFFSET = '!'.charCodeAt(0);
+
+exports.run = (bot, msg, args) => {
+    if (args.length < 1) {
+        throw 'You must provide text to flip!';
+    }
+
+    msg.edit(
+        args.join(' ').split('')
+            .map(c => c.charCodeAt(0) - OFFSET)
+            .map(c => mapping[c] || ' ')
+            .reverse().join('')
+    );
+};
+
+exports.info = {
+    name: 'fliptext',
+    usage: 'fliptext <text>',
+    description: 'Flips your text!'
+};
