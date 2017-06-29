c0e9b327d374588f8db045a6f16b4451b876dc58
nobobo1234 <noahvanboven@gmail.com>
2017-06-29 20:46:49 +0200

    "Beautified" undelete command

--- src/commands/Info/undelete.js
+++ src/commands/Info/undelete.js
@@ -1,11 +1,17 @@
 exports.run = function (bot, msg, args) {
-    let id = msg.channel.type === 'text' && msg.mentions.users.size ? msg.mentions.users.first().id : args[0];
-    if (msg.channel.type === 'text' && args[0].replace(/!/g, '') !== msg.mentions.users.first().toString()) id = args[0];
-    const delmsg = bot.deleted.get(id);
-    if(!delmsg) throw 'No recently deleted messages found';
-    bot.deleted.delete(id);
-    const text = `Undeleted message of ${msg.mentions.users.first().name} in ${delmsg.guild.name} | ${delmsg.channel.name}\n\`\`\`${delmsg.content}\`\`\``;
-    msg.edit(text);
+    const user = msg.mentions.users.first();
+    if (!user) {
+        throw 'Please mention a user.';
+    }
+
+    const delmsg = bot.deleted.get(user.id);
+    if (!delmsg) {
+        throw 'No recently deleted messages found';
+    }
+
+    bot.deleted.delete(user.id);
+
+    msg.edit(`Undeleted message of ${msg.mentions.users.first().name} in ${delmsg.guild.name} | ${delmsg.channel.name}\n\`\`\`${delmsg.content}\`\`\``);
 };
 
 exports.info = {
