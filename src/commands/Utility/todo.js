959c0dab748360742732d87c560a72ac797d9660
Yoni Lerner <yonilerner@gmail.com>
2017-07-09 17:22:45 -0700

    Todos done

--- src/commands/Utility/todo.js
+++ src/commands/Utility/todo.js
@@ -89,7 +89,7 @@ class TodoLists {
         if (!this.getList(currentList)) {
             currentList = TodoLists.mainListName;
         }
-        this.currentList = currentList;
+        this.currentListName = currentList;
     }
 
     /**
@@ -97,11 +97,11 @@ class TodoLists {
      * @returns {TodoList}
      */
     getCurrentList() {
-        return this.getList(this.currentList);
+        return this.getList(this.currentListName);
     }
 
     save() {
-        this.storage.set(TodoLists.currentListStorageKey, this.currentList);
+        this.storage.set(TodoLists.currentListStorageKey, this.currentListName);
         this.storage.set(TodoLists.listsStorageKey, this.lists);
         this.storage.save();
     }
@@ -110,26 +110,42 @@ class TodoLists {
         return this.lists[name];
     }
 
+    getListNames() {
+        return Object.keys(this.lists);
+    }
+
     deleteList(name) {
         if (!this.getList(name)) {
-            throw `List ${name} does not exist!`;
+            throw `List '${name}' does not exist!`;
         }
         if (name === TodoLists.mainListName) {
-            throw `List ${name} cannot be deleted!`;
+            throw `List '${name}' cannot be deleted!`;
         }
         delete this.lists[name];
+
+        if (this.currentListName === name) {
+            this.switchList(TodoLists.mainListName);
+        }
     }
 
     createList(name) {
         if (this.getList(name)) {
-            throw `List ${name} already exists!`;
+            throw `List'${name}' already exists!`;
         }
         this.lists[name] = new TodoList(name);
+        this.switchList(name);
+    }
+
+    switchList(name) {
+        if (!this.getList(name)) {
+            throw `List '${name}' does not exist!`;
+        }
+        this.currentListName = name;
     }
 }
 
 TodoLists.listsStorageKey = 'lists';
-TodoLists.currentListStorageKey = 'currentList';
+TodoLists.currentListStorageKey = 'currentListName';
 TodoLists.mainListName = 'Main';
 
 module.exports.run = (bot, msg, args) => {
@@ -137,23 +153,45 @@ module.exports.run = (bot, msg, args) => {
         embed: bot.utils.embed('Todo List Utility', `${prefix}${message}`)
     });
 
-
     if (args.length < 1) {
         return send(Lists.getCurrentList(), '');
     }
 
-    const {options, leftover} = bot.utils.parseArgs(args, ['d', 'c', 'i']);
+    const {options, leftover} = bot.utils.parseArgs(args, ['d', 'c', 'i', 'l', 'n', 'r', 's']);
     let indexOption = parseInt(leftover[0], 10);
 
+    const ensureProperNumberOfArgs = (max, message = 'Expected list item number only', min = 1) => {
+        if (leftover.length > max || leftover.length < min) {
+            throw message;
+        }
+    };
+
     if (options.c) {
+        ensureProperNumberOfArgs(1);
         Lists.getCurrentList().markComplete(indexOption);
         send(`Item ${indexOption} marked complete!`);
     } else if (options.i) {
+        ensureProperNumberOfArgs(1);
         Lists.getCurrentList().markComplete(indexOption, false);
         send(`Item ${indexOption} marked incomplete!`);
     } else if (options.d) {
+        ensureProperNumberOfArgs(1);
         Lists.getCurrentList().remove(indexOption);
         send(`Item ${indexOption} deleted!`);
+    } else if (options.l) {
+        send(`Available lists: ${Lists.getListNames().join(', ')}`);
+    } else if (options.n) {
+        ensureProperNumberOfArgs(1, 'Expected name of new list to be a single word');
+        Lists.createList(leftover[0]);
+        send(`List '${leftover[0]}' created!`);
+    } else if (options.r) {
+        ensureProperNumberOfArgs(1, 'Expected name of list to delete to be a single word');
+        Lists.deleteList(leftover[0]);
+        send(`List '${leftover[0]}' deleted!`);
+    } else if (options.s) {
+        ensureProperNumberOfArgs(1, 'Expected name of chosen list to be a single word');
+        Lists.switchList(leftover[0]);
+        send(`Switched to list '${leftover[0]}'`);
     } else {
         Lists.getCurrentList().add(leftover.join(' '));
         send('Item added!');
@@ -172,7 +210,43 @@ module.exports.init = bot => {
 
 module.exports.info = {
     name: 'todo',
-    usage: 'todo', // TODO
-    description: 'Manage a todo list',
-    options: []
+    usage: 'todo | todo [options...] <index> | todo <text of item to add>', // TODO
+    description: 'Manage a todo list. The command by itself will show the list.',
+    options: [
+        {
+            name: '-c',
+            usage: '-c ',
+            description: 'Mark an item complete'
+        },
+        {
+            name: '-i',
+            usage: '-i ',
+            description: 'Mark an item incomplete'
+        },
+        {
+            name: '-d',
+            usage: '-d',
+            description: 'Delete an item'
+        },
+        {
+            name: '-l',
+            usage: '-l',
+            description: 'Show all lists'
+        },
+        {
+            name: '-n',
+            usage: '-n ',
+            description: 'Make a new list and set it as the current list'
+        },
+        {
+            name: '-r',
+            usage: '-r ',
+            description: 'Remove a list. If the list being removed is the list currently in use, automatically switches back to the Main list.'
+        },
+        {
+            name: '-s',
+            usage: '-s ',
+            description: 'Switch lists'
+        }
+    ]
 };
