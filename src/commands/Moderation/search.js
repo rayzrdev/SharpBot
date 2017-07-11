cd3d3a7165f86e2fef74c694388a573459fe056d
Rayzr522 <rayzr522@gmail.com>
2017-07-11 18:37:56 -0400

    Updated //search

--- src/commands/Moderation/search.js
+++ src/commands/Moderation/search.js
@@ -1,24 +1,36 @@
-exports.run = (bot, msg, args) => {
+exports.run = async (bot, msg, args) => {
     if (args.length < 1) {
         throw 'You must specify what to search for!';
     }
 
     let query = args.join(' ');
 
-    msg.edit(`:arrows_counterclockwise: Searching the last \`100\` messages for \`${query}\``)
-        .then(m => {
+    await msg.edit(`:arrows_counterclockwise: Searching the last \`100\` messages for \`${query}\``);
 
-            msg.channel.fetchMessages({ limit: 100, before: msg.id })
-                .then(messages => {
-                    let results = messages.filter(it => it.content.toLowerCase().indexOf(query.toLowerCase()) != -1);
-                    let output = results
-                        .map(it => `${formatDate(it.createdAt)} ${it.author.username}: ${it.content}`)
-                        .join('\n');
-                    m.editCode('log', output);
+    const messages = await msg.channel.fetchMessages({ limit: 100, before: msg.id });
 
-                }).catch(msg.error);
-        });
+    const results = messages.filter(it => it.cleanContent.toLowerCase().indexOf(query.toLowerCase()) != -1)
+        .sort((a, b) => (a.editedTimestamp || a.createdTimestamp) - (b.editedTimestamp || b.createdTimestamp));
 
+    if (results.length < 1) {
+        throw 'No results found.';
+    }
+
+    const output = results
+        .map(message => `${formatDate(message.createdAt)} ${message.author.username}: ${message.content}`)
+        .join('\n');
+
+    msg.delete();
+    bot.utils.sendLarge(
+        msg.channel,
+        output.replace(/`/g, '\u200b`'),
+        {
+            prefix: '\`\`\`log\n',
+            suffix: '\n```',
+            delay: 10,
+            cutOn: '\n'
+        }
+    );
 };
 
 function formatDate(date) {
