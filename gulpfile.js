8e1772e37d67ca0472080c27840cbb6b1f5a5d5c
Rayzr522 <rayzr522@gmail.com>
2017-07-12 10:32:05 -0400

    Tweaked gulpfile checks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 // The bot will have other problems if they're using Node v6<, so the only version
 // we probably need to worry about is v6.
-if (process.version.startsWith('v6')) {
+if (process.version.startsWith('v6') || typeof Object.values === 'undefined') {
     console.error('[ERROR] SharpBot requires Node v7 or greater. Please download it at https://nodejs.org/en/download/current.');
-    console.error('- Windows - https://nodejs.org/en/download/current');
-    console.error('- macOS - https://nodejs.org/en/download/current');
-    console.error('- Linux - https://nodejs.org/en/download/package-manager');
+    console.error('| Windows | https://nodejs.org/en/download/current');
+    console.error('|  macOS  | https://nodejs.org/en/download/current');
+    console.error('|  Linux  | https://nodejs.org/en/download/package-manager');
     process.exit(1);
 }
 
-const moduleError = /Error: Cannot find module '([a-zA-Z0-9+_-]+)'/g;
+const moduleError = /Error: Cannot find module '([a-zA-Z0-9+_-]+)'/;
 
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
