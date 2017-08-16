81748b026fb8a8c6c964f777c5e64726c83c86f0
nobobo1234 <noahvanboven@gmail.com>
2017-08-16 17:36:43 +0200

    Improving and fully editing, and minimizing google command to parse google cards

--- src/commands/Utility/google.js
+++ src/commands/Utility/google.js
@@ -1,60 +1,72 @@
-const got = require('got');
 const cheerio = require('cheerio');
-
-const QUERY_STRING_SETTINGS = [
-    'client=chrome',
-    'rls=en',
-    'ie=UTF-8',
-    'oe=UTF-8'
-].join('&');
-
-function getText(children) {
-    if (children.children) return getText(children.children);
-    return children.map(c => {
-        return c.children ? getText(c.children) : c.data;
-    }).join('');
-}
+const snekfetch = require('snekfetch');
+const { stringify } = require('querystring');
+const { RichEmbed } = require('discord.js');
 
 exports.run = async (bot, msg, args) => {
-    if (args.length < 1) {
-        throw 'You must enter something to search for!';
-    }
+    if(args.length < 1) throw 'I need to know what to search...';
 
-    await msg.edit(':arrows_counterclockwise: Searching...');
+    const params = {
+        q: args.join(' '),
+        safe: 'on',
+        lr: 'lang_en',
+        hl: 'en'
+    };
 
-    const res = await got(`https://google.com/search?${QUERY_STRING_SETTINGS}&q=${encodeURIComponent(args.join(' '))}`);
-    if (res.statusCode !== 200) {
-        return msg.edit(`:no_entry_sign: Error! (${res.statusCode}): ${res.statusMessage}`);
-    }
+    let resp = await snekfetch.get('https://google.com/search?' + stringify(params)).set('User-Agent', 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) Gecko/20100101 Firefox/53.0');
 
-    let $ = cheerio.load(res.body);
+    if(resp.status !== 200) throw 'Google didn\'t want to respond somehow';
+
+    let $ = cheerio.load(resp.body);
+
     let results = [];
 
-    $('.g').each((i) => {
-        results[i] = {};
+    let card = null;
+
+    let cardNode = $('div#rso > div._NId').find('div.vk_c, div.g.mnr-c.g-blk, div.kp-blk');
+
+    if(!cardNode || cardNode.length === 0) card = null;
+    else card = bot.utils.parseCards($, cardNode);
+
+    $('.rc > h3 > a').each((i, e) => {
+        const link = $(e).attr('href');
+        const text = $(e).text();
+        if(link) {
+            results.push({ text : text, link: link });
+        }
     });
 
-    $('.g>.r>a').each((i, e) => {
-        let raw = e.attribs['href'];
-        results[i]['link'] = decodeURIComponent(raw.substr(7, raw.indexOf('&sa=U') - 7));
-    });
+    if(card) {
+        const value = results.slice(0, 3).map(r => `[${r.text}](${r.link})`).join('\n');
+        if(value) {
+            card.addField(`This is what I also found for: "${params.q}" `, value)
+                .setColor(bot.utils.randomColor())
+                .setURL(`https://google.com/search?q=${encodeURIComponent(params.q)}`);
+        }
+        return await msg.channel.send({ embed: card });
+    }
 
-    $('.g>.s>.st').each((i, e) => {
-        results[i]['description'] = getText(e);
-    });
+    if(results.length === 0) {
+        return await msg.channel.send('Sorry, we didn\'t found any results');
+    }
 
-    let output = results.filter(r => r.link && r.description)
-        .slice(0, 5)
-        .map(r => `${r.link}\n\t${r.description}\n`)
-        .join('\n');
+    let nexttwo = results.slice(1, 3).map(r => `[${r.text}](${r.link})`).join('\n');
+    let firstentry = `[${results[0].text}](${results[0].link})`;
 
-    msg.edit({
-        embed: bot.utils.embed(`Search results for \`"${args.join(' ')}"\``, output)
-    });
+
+    const embed = bot.utils.embed(`Search results for ${args.join(' ')}`, firstentry, [
+        {
+            name: 'More links',
+            value: nexttwo
+        }
+    ]).setURL(`https://google.com/search?q=${encodeURIComponent(params.q)}`);
+    await msg.delete();
+    await msg.channel.send({embed: embed});
+
 };
 
 exports.info = {
     name: 'google',
-    usage: 'google <search>',
-    description: 'Searches Google using magic'
+    usage: 'google <query>',
+    description: 'Searches google using magic :O'
 };
