a1e4dbba12ddaccc24e5735faf15554f2745674b
Maurice Schmitz <Doxylamin@users.noreply.github.com>
2017-11-11 09:42:10 +0100

    fixed indent, now using lambda notation

--- src/commands/Utility/spotify.js
+++ src/commands/Utility/spotify.js
@@ -4,7 +4,7 @@ const SpotifyUriParser = require('spotify-uri');
 const got = require('got');
 const spotifyApi = new SpotifyWebApi();
 
-exports.run = async function (bot, msg, args) {
+exports.run = async (bot, msg, args) => {
     await got('https://spotify-auth.doxylamin.pw/').then((response) => {
         spotifyApi.setAccessToken(response.body);
     });
@@ -13,10 +13,10 @@ exports.run = async function (bot, msg, args) {
         throw 'You must specify a spotify uri at least!';
     }
 
-    let parsed = bot.utils.parseArgs(args, ['preview']);
-    let spotifyUri = SpotifyUriParser.parse(parsed.leftover.join(' '));
+    const parsed = bot.utils.parseArgs(args, ['preview']);
+    const spotifyUri = SpotifyUriParser.parse(parsed.leftover.join(' '));
 
-    switch(spotifyUri.type) {
+    switch (spotifyUri.type) {
     case 'track':
         getTrackEmbed(msg, spotifyUri.id, parsed.options.preview);
         break;
@@ -34,88 +34,84 @@ exports.run = async function (bot, msg, args) {
 
 function getPlaylistEmbed(msg, user, spotifyId, withPreview) {
     spotifyApi.getPlaylist(user, spotifyId)
-    .then(function(data) {
-        let apiResponse = data.body;
-        
-        const embed = new RichEmbed()
-        .setColor([30,215,96])
-        .setThumbnail(apiResponse.images[0].url)
-        .setAuthor('Click to listen on Spotify','https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
-        .addField('Playlist Name', apiResponse.name, true)
-        .addField('Created by', apiResponse.owner.display_name, true)
-        .addField('Description', apiResponse.description, true)
-        .addField('Public Playlist', ((apiResponse.public)?'Yes':'No'), true)
-        .addField('Collaborative', ((apiResponse.collaborative)?'Yes':'No'), true)
-        .addField('Followers', apiResponse.followers.total, true)
-        .addField('Track Count', apiResponse.tracks.total, true);
+        .then((data) => {
+            const apiResponse = data.body;
 
-        msg.edit({content:'',embed:embed});
-        
-        if(withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
+            const embed = new RichEmbed()
+                .setColor([30, 215, 96])
+                .setThumbnail(apiResponse.images[0].url)
+                .setAuthor('Click to listen on Spotify', 'https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
+                .addField('Playlist Name', apiResponse.name, true)
+                .addField('Created by', apiResponse.owner.display_name, true)
+                .addField('Description', apiResponse.description, true)
+                .addField('Public Playlist', ((apiResponse.public) ? 'Yes' : 'No'), true)
+                .addField('Collaborative', ((apiResponse.collaborative) ? 'Yes' : 'No'), true)
+                .addField('Followers', apiResponse.followers.total, true)
+                .addField('Track Count', apiResponse.tracks.total, true);
 
-    }, function(err) {
-        throw `Something went wrong! ${err}`;
-    });
+            msg.edit({ embed });
+
+            if (withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
+
+        }).catch((err) => {
+            throw `Something went wrong! ${err}`;
+        });
 }
 
 function getArtistEmbed(msg, spotifyId, withPreview) {
     spotifyApi.getArtist(spotifyId)
-    .then(function(data) {
-        let apiResponse = data.body;
-        
-        const embed = new RichEmbed()
-        .setColor([30,215,96])
-        .setThumbnail(apiResponse.images[0].url)
-        .setAuthor('Click to listen on Spotify','https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
-        .addField('Artist Name', apiResponse.name, true)
-        .addField('Followers',  apiResponse.followers.total.toLocaleString(), true)
-        .addField('Popularity',  apiResponse.popularity + '%', true)
-        .addField('Genres',  ((apiResponse.genres.length > 0)?apiResponse.genres.join(', '):'unknown'), true);
+        .then((data) => {
+            const apiResponse = data.body;
 
-        msg.edit({content:'',embed:embed});
-        
-        if(withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
+            const embed = new RichEmbed()
+                .setColor([30, 215, 96])
+                .setThumbnail(apiResponse.images[0].url)
+                .setAuthor('Click to listen on Spotify', 'https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
+                .addField('Artist Name', apiResponse.name, true)
+                .addField('Followers', apiResponse.followers.total.toLocaleString(), true)
+                .addField('Popularity', apiResponse.popularity + '%', true)
+                .addField('Genres', ((apiResponse.genres.length > 0) ? apiResponse.genres.join(', ') : 'unknown'), true);
 
-    }, function(err) {
-        throw `Something went wrong! ${err}`;
-    });
+            msg.edit({ embed });
+
+            if (withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
+
+        }).catch((err) => {
+            throw `Something went wrong! ${err}`;
+        });
 }
 
 function getTrackEmbed(msg, spotifyId, withPreview) {
     spotifyApi.getTrack(spotifyId)
-    .then(function(data) {
-        let apiResponse = data.body;
-        let artists = [];
+        .then((data) => {
+            const apiResponse = data.body;
+            let artists = apiResponse.artists.map(artist => artist.name);
 
-        apiResponse.artists.forEach(function(element) {
-            artists.push(element.name);
-        }, this);
+            const embed = new RichEmbed()
+                .setColor([30, 215, 96])
+                .setThumbnail(apiResponse.album.images[0].url)
+                .setAuthor('Click to listen on Spotify', 'https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
+                .addField('Artist', artists.join(', '), true)
+                .addField('Title', apiResponse.name, true)
+                .addField('Length', calculateDuration(apiResponse.duration_ms), true)
+                .addField('Album', apiResponse.album.name, true)
+                .addField('Parental Advisory', ((apiResponse.explicit) ? 'contains explicit lyrics' : 'none applicable'), true)
+                .addField('Availability', `Available in ${apiResponse.available_markets.length} countrys`, true)
+                .addField('Popularity', `${apiResponse.popularity}%`, true);
 
-        const embed = new RichEmbed()
-        .setColor([30,215,96])
-        .setThumbnail(apiResponse.album.images[0].url)
-        .setAuthor('Click to listen on Spotify','https://image.flaticon.com/icons/png/512/174/174872.png', apiResponse.external_urls.spotify)
-        .addField('Artist', artists.join(', '), true)
-        .addField('Title',  apiResponse.name, true)
-        .addField('Length', calculateDuration(apiResponse.duration_ms), true)
-        .addField('Album',  apiResponse.album.name, true)
-        .addField('Parental Advisory',  ((apiResponse.explicit)?'contains explicit lyrics':'none applicable'), true)
-        .addField('Availability', `Available in ${apiResponse.available_markets.length} countrys`, true)
-        .addField('Popularity', `${apiResponse.popularity}%`, true);
-        
-        msg.edit({embed:embed});
+            msg.edit({ embed: embed });
 
-        if(withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
+            if (withPreview) msg.channel.send(`Got spotify? Click below to play! ${apiResponse.external_urls.spotify}`);
 
-    }, function(err) {
-        throw `Something went wrong! ${err}`;
-    });
+        }).catch((err) => {
+            throw `Something went wrong! ${err}`;
+        });
 }
 
 function calculateDuration(ms) {
     const minutes = Math.floor(ms / 60000);
     const seconds = ((ms % 60000) / 1000).toFixed(0);
-    return (seconds == 60 ? (minutes+1) + ':00' : minutes + ':' + (seconds < 10 ? '0' : '') + seconds);
+    return (seconds == 60 ? (minutes + 1) + ':00' : minutes + ':' + (seconds < 10 ? '0' : '') + seconds);
 }
 
 exports.info = {
