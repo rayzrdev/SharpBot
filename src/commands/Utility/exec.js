12aa9f7784319bc5659af0f0bf811aadff110633
Rayzr522 <rayzr522@gmail.com>
2017-11-28 17:06:02 -0500

    Massively improved the //exec command

--- src/commands/Utility/exec.js
+++ src/commands/Utility/exec.js
@@ -1,11 +1,15 @@
 const { exec } = require('child_process');
 const username = require('os').userInfo().username;
 
-exports.run = (bot, msg, args) => {
-    let parsed = bot.utils.parseArgs(args, 's', 'l:');
+exports.run = async (bot, msg, args) => {
+    let parsed = bot.utils.parseArgs(args, ['r', 'd', 's', 'f', 'fn:', 'l:']);
 
     if (parsed.length < 1) {
         throw 'You must provide a command to run!';
+    }
+
+    if (parsed.options.d) {
+        msg.delete();
     }
 
     let ps = exec(parsed.leftover.join(' '));
@@ -18,19 +22,52 @@ exports.run = (bot, msg, args) => {
     }
 
     let opts = {
-        prefix: `\`\`\`${parsed.options.l || 'bash'}\n`,
-        suffix: '\n```',
         delay: 10,
         cutOn: '\n'
     };
 
-    ps.stdout.on('data', data => bot.utils.sendLarge(msg.channel, clean(data), opts));
-    ps.stderr.on('data', data => bot.utils.sendLarge(msg.channel, clean(data), opts));
+    if (!parsed.options.r) {
+        opts.prefix = `\`\`\`${parsed.options.l || 'bash'}\n`;
+        opts.suffix = '\n```';
+    }
+
+    if (parsed.options.f) {
+        let output = '';
+
+        ps.stdout.on('data', data => output += data.toString());
+        await new Promise(resolve => {
+            ps.once('exit', async () => {
+                if (!output) {
+                    return resolve();
+                }
+
+                try {
+                    await msg.channel.send({
+                        files: [
+                            {
+                                attachment: output.replace(/^file:\/\//, ''),
+                                name: parsed.options.fn
+                            }
+                        ]
+                    });
+                } catch (err) {
+                    msg.error('Invalid URL/path!');
+                }
+
+                resolve();
+            });
+        });
+    } else {
+        ps.stdout.on('data', data => bot.utils.sendLarge(msg.channel, clean(data), opts));
+        ps.stderr.on('data', data => bot.utils.sendLarge(msg.channel, clean(data), opts));
+
+        await new Promise(resolve => ps.once('exit', resolve));
+    }
 };
 
 const clean = function (data) {
     return `${data}`
-        .replace(/`/g, '\\$&')
+        .replace(/`/g, '\u200b$&')
         .replace(new RegExp(username, 'g'), '<Hidden>')
         .replace(/\[[0-9]*m/g, '');
 };
@@ -48,6 +85,23 @@ exports.info = {
             name: '-l',
             usage: '-l <lang>',
             description: 'Sets the language of the outputted code block'
+        },
+        {
+            name: '-r',
+            description: 'Sends the output raw, without any code blocks'
+        },
+        {
+            name: '-d',
+            description: 'Deletes the command message'
+        },
+        {
+            name: '-f',
+            description: 'Interperts the response as a file URL/path to send'
+        },
+        {
+            name: '-fn',
+            usage: '-fn <name>',
+            description: 'Sets the name for the sent file'
         }
     ]
 };
