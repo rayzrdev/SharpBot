04458731543b5f5d005fb9d3a019dfca3452a63d
Rayzr <rayzr522@gmail.com>
2018-01-06 11:26:46 -0500

    Added country flag to the //weather command

--- src/commands/Utility/weather.js
+++ src/commands/Utility/weather.js
@@ -1,4 +1,5 @@
 const got = require('got');
+const countries = require('country-data').countries.all;
 
 const makeURL = (city) => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(city)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 const celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
@@ -23,9 +24,12 @@ exports.run = async (bot, msg, args) => {
     const weatherInfo = res.body.query.results.channel;
     const forecast = weatherInfo.item.forecast[0];
 
+    const countryInfo = countries.find(country => country.name === weatherInfo.location.country);
+    const countryEmoji = countryInfo ? countryInfo.emoji : ':grey_question:';
+
     const description = `The current temperature in ${weatherInfo.location.city} is ${weatherInfo.item.condition.temp}°F/${celsius(weatherInfo.item.condition.temp)}°C`;
 
-    const embed = bot.utils.embed(weatherInfo.item.title, description, [
+    const embed = bot.utils.embed(`${countryEmoji} ${weatherInfo.item.title}`, description, [
         {
             name: 'Condition',
             value: weatherInfo.item.condition.text
