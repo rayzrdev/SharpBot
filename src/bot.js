4e98512d355673723f10b0e5273c5dc2ead3ec18
Rayzr522 <rayzr522@gmail.com>
2018-03-12 23:40:39 -0400

    Separated bot.js into class and initialization

--- src/bot.js
+++ src/bot.js
@@ -2,12 +2,10 @@
 
 const fse = require('fs-extra');
 const envPaths = require('env-paths');
-const Discord = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const stripIndents = require('common-tags').stripIndents;
 const chalk = require('chalk');
 const Managers = require('./managers');
-
-const bot = global.bot = exports.client = new Discord.Client();
 
 // Settings
 
@@ -21,135 +19,150 @@ const settings = global.settings = {
 if (!fse.existsSync(settings.dataFolder)) fse.mkdirSync(settings.dataFolder);
 if (!fse.existsSync(settings.configsFolder)) fse.mkdirSync(settings.configsFolder);
 
-// Managers
+class SharpBot extends Client {
+    constructor(config = {}) {
+        super();
 
-bot.managers = {};
+        global.bot = this;
 
-const logger = bot.logger = new Managers.Logger(bot);
-logger.inject();
+        // Managers
+        this.managers = {};
 
-bot.managers.dynamicImports = global.dynamicImports = new Managers.DynamicImports(bot, __dirname);
-bot.managers.dynamicImports.init();
+        // Logger
+        const logger = this.logger = new Managers.Logger(this);
+        logger.inject();
 
-const configManager = bot.managers.config = new Managers.Config(bot, __dirname, bot.managers.dynamicImports);
+        // Dynamic imports
+        this.managers.dynamicImports = new Managers.DynamicImports(this, __dirname);
+        this.managers.dynamicImports.init();
 
-bot.config = global.config = configManager.load();
+        // Config
+        this.managers.config = new Managers.Config(this, __dirname, this.managers.dynamicImports);
+        this.config = global.config = this.managers.config.load();
 
-const pluginManager = bot.plugins = bot.managers.pluginManager = new Managers.Plugins(bot);
-pluginManager.loadPlugins();
+        // Plugins
+        this.plugins = this.managers.pluginManager = new Managers.Plugins(this);
+        this.plugins.loadPlugins();
 
-bot.storage = new Managers.Storage();
+        // Storage
+        this.storage = new Managers.Storage();
 
-bot.managers.notifications = new Managers.Notifications(bot);
+        // Notifications
+        this.managers.notifications = new Managers.Notifications(this);
 
-const commands = bot.commands = new Managers.CommandManager(bot);
-const stats = bot.managers.stats = new Managers.Stats(bot);
+        // Commands
+        const commands = this.commands = new Managers.CommandManager(this);
 
-Managers.Migrator.migrate(bot);
+        // Stats
+        const stats = this.managers.stats = new Managers.Stats(this);
 
-// Deleted message record handler
+        Managers.Migrator.migrate(this);
 
-bot.deleted = new Discord.Collection();
+        // Deleted message record handler
+        this.deleted = new Collection();
+        this.setInterval(() => {
+            this.deleted.clear();
+        }, 7200000);
 
-bot.setInterval(() => {
-    bot.deleted.clear();
-}, 7200000);
+        // Uncategorized
+        this.loaded = false;
+        this.utils = global.utils = require('./utils');
 
-// Uncategorized
+        // Event listeners
+        this.on('ready', () => {
+            if (this.user.bot) {
+                logger.severe(`SharpBot is a selfbot, but you entered a bot token. Please follow the instructions at ${chalk.green('https://github.com/RayzrDev/SharpBot#getting-your-user-token')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
+                process.exit(666);
+            }
 
-let loaded = false;
+            // Fix mobile notifications
+            this.user.setAFK(true);
 
-bot.utils = global.utils = require('./utils');
+            commands.loadCommands();
 
-// Event listeners
+            (title => {
+                process.title = title;
+                process.stdout.write(`\u001B]0;${title}\u0007`);
+            })(`SharpBot - ${this.user.username}`);
 
-bot.on('ready', () => {
-    if (bot.user.bot) {
-        logger.severe(`SharpBot is a selfbot, but you entered a bot token. Please follow the instructions at ${chalk.green('https://github.com/RayzrDev/SharpBot#getting-your-user-token')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
-        process.exit(666);
+            logger.info(stripIndents`Stats:
+            - User: ${this.user.username}#${this.user.discriminator} <ID: ${this.user.id}>
+            - Prefix: ${this.config.prefix}
+            - Users: ${this.users.filter(user => !user.bot).size}
+            - Bots: ${this.users.filter(user => user.bot).size}
+            - Channels: ${this.channels.size}
+            - Guilds: ${this.guilds.size}`
+            );
+
+            stats.set('start-time', process.hrtime());
+
+            delete this.user.email;
+            delete this.user.verified;
+
+            logger.info('Bot loaded');
+
+            this.loaded = true;
+        });
+
+        this.on('message', msg => {
+            stats.increment(`messages-${this.user.id === msg.author.id ? 'sent' : 'received'}`);
+            if (msg.isMentioned(this.user)) {
+                stats.increment('mentions');
+                console.log(`[MENTION] ${msg.author.username} | ${msg.guild ? msg.guild.name : '(DM)'} | #${msg.channel.name || 'N/A'}:\n${msg.cleanContent}`);
+            }
+
+            if (msg.author.id !== this.user.id) return;
+
+            if (msg.guild && this.config.blacklistedServers && this.config.blacklistedServers.indexOf(msg.guild.id.toString()) > -1) {
+                return;
+            }
+
+            return this.commands.handleCommand(msg, msg.content);
+        });
+
+        this.on('messageDelete', msg => this.deleted.set(msg.author.id, msg));
+
+        this.on('error', console.error);
+        this.on('warn', console.warn);
+        this.on('disconnect', event => {
+            if (event.code === 1000) {
+                this.logger.info('Disconnected from Discord cleanly');
+            } else if (event.code === 4004) {
+                // Force the user to reconfigure if their token is invalid
+                this.logger.severe(`Failed to authenticate with Discord. Please follow the instructions at ${chalk.green('https://github.com/RayzrDev/SharpBot#getting-your-user-token')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
+                process.exit(666);
+            } else {
+                this.logger.warn(`Disconnected from Discord with code ${event.code}`);
+            }
+        });
+
+        // Process handlers
+        process.on('exit', () => {
+            this.storage.saveAll();
+            this.loaded && this.destroy();
+        });
+
+        process.on('uncaughtException', (err) => {
+            let errorMsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}\/`, 'g'), './');
+            this.logger.severe(errorMsg);
+        });
+
+        process.on('unhandledRejection', err => {
+            this.logger.severe('Uncaught Promise error: \n' + err.stack);
+        });
     }
 
-    // =======================================================
-    // === Until we know how to fix this, just make people ===
-    // === use the //status command to make the bot invis. ===
-    // =======================================================
-    // bot.user.setStatus('invisible');
-
-    // Fix mobile notifications
-    bot.user.setAFK(true);
-
-    commands.loadCommands();
-
-    (title => {
-        process.title = title;
-        process.stdout.write(`\u001B]0;${title}\u0007`);
-    })(`SharpBot - ${bot.user.username}`);
-
-    logger.info(stripIndents`Stats:
-        - User: ${bot.user.username}#${bot.user.discriminator} <ID: ${bot.user.id}>
-        - Prefix: ${bot.config.prefix}
-        - Users: ${bot.users.filter(user => !user.bot).size}
-        - Bots: ${bot.users.filter(user => user.bot).size}
-        - Channels: ${bot.channels.size}
-        - Guilds: ${bot.guilds.size}`
-    );
-
-    stats.set('start-time', process.hrtime());
-
-    delete bot.user.email;
-    delete bot.user.verified;
-
-    logger.info('Bot loaded');
-
-    loaded = true;
-});
-
-bot.on('message', msg => {
-    stats.increment(`messages-${bot.user.id === msg.author.id ? 'sent' : 'received'}`);
-    if (msg.isMentioned(bot.user)) {
-        stats.increment('mentions');
-        console.log(`[MENTION] ${msg.author.username} | ${msg.guild ? msg.guild.name : '(DM)'} | #${msg.channel.name || 'N/A'}:\n${msg.cleanContent}`);
+    start() {
+        if (this.config) {
+            this.login(this.config.botToken);
+            return true;
+        } else {
+            return false;
+        }
     }
+}
 
-    if (msg.author.id !== bot.user.id) return;
 
-    if (msg.guild && bot.config.blacklistedServers && bot.config.blacklistedServers.indexOf(msg.guild.id.toString()) > -1) {
-        return;
-    }
-
-    return bot.commands.handleCommand(msg, msg.content);
-});
-
-bot.on('messageDelete', (msg) => {
-    bot.deleted.set(msg.author.id, msg);
-});
-
-process.on('exit', () => {
-    bot.storage.saveAll();
-    loaded && bot.destroy();
-});
-
-bot.on('error', console.error);
-bot.on('warn', console.warn);
-bot.on('disconnect', event => {
-    if (event.code === 1000) {
-        logger.info('Disconnected from Discord cleanly');
-    } else if (event.code === 4004) {
-        // Force the user to reconfigure if their token is invalid
-        logger.severe(`Failed to authenticate with Discord. Please follow the instructions at ${chalk.green('https://github.com/RayzrDev/SharpBot#getting-your-user-token')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
-        process.exit(666);
-    } else {
-        logger.warn(`Disconnected from Discord with code ${event.code}`);
-    }
-});
-
-process.on('uncaughtException', (err) => {
-    let errorMsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}\/`, 'g'), './');
-    logger.severe(errorMsg);
-});
-
-process.on('unhandledRejection', err => {
-    logger.severe('Uncaught Promise error: \n' + err.stack);
-});
-
-bot.config && bot.login(bot.config.botToken);
+// Temporary: We're eventually gonna have a CLI-based launcher for this.
+exports.client = new SharpBot();
+exports.client.start();
